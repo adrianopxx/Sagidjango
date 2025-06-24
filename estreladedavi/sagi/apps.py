0fbbb8138976d014986d6e310fd67ea76cc2da30
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2025-06-24 13:24:58 -0300

    Delete estreladedavi directory

--- estreladedavi/sagi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SagiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sagi'
--- estreladedavi/sagi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),          # rota principal "/"
-    path('sobre/', views.sobre, name='sobre'),  # rota "/sobre/"
-]
